c7f392d89010c6a542281d2c883261aafc5a081a
roizwaelah <roizwaelah@gmail.com>
2026-03-16 05:46:52 +0700

    Fix schema URI warning in activity_product layout

--- app/src/main/java/com/daspos/feature/transaction/CartAdapter.java
+++ app/src/main/java/com/daspos/feature/transaction/CartAdapter.java
@@ -3,6 +3,7 @@ package com.daspos.feature.transaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,7 +17,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CartAdapter extends RecyclerView.Adapter<CartAdapter.VH> {
+    public interface Listener {
+        void onMinus(CartItem item);
+        void onPlus(CartItem item);
+    }
+
     private final List<CartItem> items = new ArrayList<>();
+    private final Listener listener;
+
+    public CartAdapter(Listener listener) {
+        this.listener = listener;
+    }
 
     public void submit(List<CartItem> list) {
         items.clear();
@@ -33,10 +44,22 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.VH> {
 
     @Override
     public void onBindViewHolder(@NonNull VH holder, int position) {
-        CartItem item = items.get(position);
+        final CartItem item = items.get(position);
         holder.tvName.setText(item.getProduct().getName());
         holder.tvQty.setText(item.getQty() + " x " + CurrencyUtils.formatRupiah(item.getProduct().getPrice()));
         holder.tvSubtotal.setText(CurrencyUtils.formatRupiah(item.getSubtotal()));
+        holder.btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) listener.onMinus(item);
+            }
+        });
+        holder.btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) listener.onPlus(item);
+            }
+        });
     }
 
     @Override
@@ -46,12 +69,15 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.VH> {
 
     static class VH extends RecyclerView.ViewHolder {
         TextView tvName, tvQty, tvSubtotal;
+        ImageButton btnMinus, btnPlus;
 
         VH(@NonNull View itemView) {
             super(itemView);
             tvName = itemView.findViewById(R.id.tvCartName);
             tvQty = itemView.findViewById(R.id.tvCartInfo);
             tvSubtotal = itemView.findViewById(R.id.tvCartSubtotal);
+            btnMinus = itemView.findViewById(R.id.btnMinus);
+            btnPlus = itemView.findViewById(R.id.btnPlus);
         }
     }
 }
